6f43af495196b4e094f1e493c259f8f9d097ba40
inci <inci@iotiq.net>
2023-06-12 14:40:10 +0300

    add password encoder bean

--- src/main/java/com/iotiq/commons/util/PasswordUtil.java
+++ src/main/java/com/iotiq/commons/util/PasswordUtil.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class PasswordUtil {
+
     private final PasswordEncoder encoder;
 
     public String encode(CharSequence rawPassword) {
